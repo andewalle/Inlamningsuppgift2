a94551f0eabbff8dfaab863d258117343d485cb6
andewalle <awtudor@gmail.com>
2018-11-19 23:33:56 +0100

    GuessingGame working from commandprompt with parameters

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
+
 public class GuessingGame {
+    Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-        Guesser guesser = new Guesser(1,1000);
+        Guesser guesser = new Guesser(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
         guesser.start();
 
     }
